ef77aa6bb3276185a98ec18dcb9d3a4c563a0cdd
Matt Monroe <matt.a.monroe@gmail.com>
2026-04-01 11:01:32 -0600

    Day 3 - Part 1 - Solution

--- 03/main.go
+++ 03/main.go
@@ -1,7 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	fmt.Printf("Hello World!")
+}
+
+func ReadBatteryBanks(content string) [][]int {
+	batteries := [][]int{}
+
+	splits := strings.Split(content, "\n")
+	for _, split := range splits {
+		if split == "" {
+			continue
+		}
+		bank := []int{}
+		for i := range split {
+			v, err := strconv.Atoi(split[i : i+1])
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "failed to process character %s: %v\n", split[i:i+1], err)
+				continue
+			}
+			bank = append(bank, v)
+		}
+		batteries = append(batteries, bank)
+	}
+
+	return batteries
+}
+
+func FindBestCombo(bank []int) int {
+	firstDigitIdx, secondDigitIdx := 0, 0
+
+	cursor := 0
+	for i, v := range bank {
+		if i == len(bank)-1 {
+			break
+		}
+		if v > cursor {
+			firstDigitIdx = i
+			cursor = v
+		}
+	}
+
+	searchLen := len(bank) - firstDigitIdx - 1
+	cursor = 0
+	for i := range searchLen {
+		loc := firstDigitIdx + i + 1
+		if bank[loc] > cursor {
+			secondDigitIdx = loc
+			cursor = bank[loc]
+		}
+	}
+
+	return bank[firstDigitIdx]*10 + bank[secondDigitIdx]
+}
+
+func SumBestCombos(batteries [][]int) int {
+	sum := 0
+	for _, bank := range batteries {
+
+		sum += FindBestCombo(bank)
+	}
+
+	return sum
 }
--- 03/main_test.go
+++ 03/main_test.go
@@ -10,11 +10,51 @@ import (
 func TestSampleP1(t *testing.T) {
 	content := aoclib.ReadFile("sample.txt")
 	assert.NotEqualf(t, "", content, "Failed to read in file contents for sample.txt")
+
+	banks := ReadBatteryBanks(content)
+
+	sum := SumBestCombos(banks)
+	assert.Equal(t, 357, sum)
 }
 
 func TestFullP1(t *testing.T) {
 	content := aoclib.ReadFile("problem.txt")
 	assert.NotEqualf(t, "", content, "Failed to read in file contents for problem.txt")
+
+	banks := ReadBatteryBanks(content)
+
+	sum := SumBestCombos(banks)
+	assert.Equal(t, 17196, sum)
+}
+
+func TestParseBatteries(t *testing.T) {
+	banks := ReadBatteryBanks("123\n456")
+	assert.Equal(t, 2, len(banks))
+	assert.Equal(t, 3, len(banks[0]))
+	assert.Equal(t, 3, len(banks[1]))
+
+	assert.Equal(t, 1, banks[0][0])
+	assert.Equal(t, 2, banks[0][1])
+	assert.Equal(t, 3, banks[0][2])
+
+	assert.Equal(t, 4, banks[1][0])
+	assert.Equal(t, 5, banks[1][1])
+	assert.Equal(t, 6, banks[1][2])
+}
+
+func TestFindBestCombo(t *testing.T) {
+	value := FindBestCombo([]int{1, 2, 3})
+	assert.Equal(t, 23, value)
+
+	value = FindBestCombo([]int{9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1})
+	assert.Equal(t, 98, value)
+	value = FindBestCombo([]int{8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9})
+	assert.Equal(t, 89, value)
+	value = FindBestCombo([]int{2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8})
+	assert.Equal(t, 78, value)
+	value = FindBestCombo([]int{8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1})
+	assert.Equal(t, 92, value)
+
 }
 
 func TestSampleP2(t *testing.T) {
