e8029bb4965811c0ff9fee5bcabc7a119f55bce4
Matt Monroe <matt.a.monroe@gmail.com>
2026-04-02 10:32:01 -0600

    Day 4 - Part 1 - Solution

--- 04/main.go
+++ 04/main.go
@@ -1,7 +1,125 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
 	fmt.Printf("Hello World!")
+}
+
+type Grid struct {
+	Rolls [][]int
+}
+
+func (g Grid) Columns() int {
+	if g.Rows() == 0 {
+		return 0
+	}
+
+	return len(g.Rolls[0])
+}
+
+func (g Grid) Rows() int {
+	return len(g.Rolls)
+}
+
+func (g *Grid) AddRow(r []int) {
+	g.Rolls = append(g.Rolls, r)
+}
+
+func (g Grid) String() string {
+	content := ""
+	for _, row := range g.Rolls {
+		for _, roll := range row {
+			if roll == 0 {
+				content += "."
+			} else {
+				content += "@"
+			}
+		}
+		content += "\n"
+	}
+	return content
+}
+
+func ReadGrid(content string) Grid {
+	grid := Grid{[][]int{}}
+
+	splits := strings.Split(content, "\n")
+	for _, split := range splits {
+		if split == "" {
+			continue
+		}
+		row := []int{}
+		for _, char := range split {
+			switch char {
+			case '.':
+				row = append(row, 0)
+			case '@':
+				row = append(row, 1)
+			default:
+				fmt.Fprintf(os.Stderr, "incorrect parsed character %c", char)
+			}
+		}
+		grid.AddRow(row)
+	}
+
+	return grid
+}
+
+func CountFreeRolls(g Grid) int {
+	countFree := 0
+	for r, row := range g.Rolls {
+		for c, value := range row {
+			if value == 0 {
+				continue
+			}
+			countNearby := 0
+			if r > 0 {
+				// row above
+				if c > 0 {
+					// top left
+					countNearby += g.Rolls[r-1][c-1]
+				}
+				// above
+				countNearby += g.Rolls[r-1][c]
+				if c < g.Columns()-1 {
+					// top right
+					countNearby += g.Rolls[r-1][c+1]
+				}
+			}
+			// left
+			if c > 0 {
+				// left
+				countNearby += g.Rolls[r][c-1]
+			}
+
+			// right
+			if c < g.Columns()-1 {
+				countNearby += g.Rolls[r][c+1]
+			}
+
+			if r < g.Rows()-1 {
+				// row below
+				if c > 0 {
+					// bottom left
+					countNearby += g.Rolls[r+1][c-1]
+				}
+				// below
+				countNearby += g.Rolls[r+1][c]
+				if c < g.Columns()-1 {
+					// bottom right
+					countNearby += g.Rolls[r+1][c+1]
+				}
+			}
+			if countNearby < 4 {
+				countFree++
+			}
+		}
+	}
+
+	return countFree
 }
--- 04/main_test.go
+++ 04/main_test.go
@@ -10,11 +10,21 @@ import (
 func TestSampleP1(t *testing.T) {
 	content := aoclib.ReadFile("sample.txt")
 	assert.NotEqualf(t, "", content, "Failed to read in file contents for sample.txt")
+
+	g := ReadGrid(content)
+
+	count := CountFreeRolls(g)
+	assert.Equal(t, 13, count)
 }
 
 func TestFullP1(t *testing.T) {
 	content := aoclib.ReadFile("problem.txt")
 	assert.NotEqualf(t, "", content, "Failed to read in file contents for problem.txt")
+
+	g := ReadGrid(content)
+
+	count := CountFreeRolls(g)
+	assert.Equal(t, 1419, count)
 }
 
 func TestSampleP2(t *testing.T) {
