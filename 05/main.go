5dbf63d0e6e895b3dcbf5b68e198684310da10b4
Matt Monroe <matt.a.monroe@gmail.com>
2026-04-06 11:33:44 -0600

    Day 5 - Part 2 - Solution

--- 05/main.go
+++ 05/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -16,6 +17,17 @@ type Range struct {
 	end   int
 }
 
+func (r Range) String() string {
+	return fmt.Sprintf("[%d - %d]", r.start, r.end)
+}
+
+func (r Range) IsIncluded(num int) bool {
+	if num >= r.start && num <= r.end {
+		return true
+	}
+	return false
+}
+
 type Ranges struct {
 	r []Range
 }
@@ -28,6 +40,15 @@ func (r Ranges) IsIncluded(num int) bool {
 	}
 
 	return false
+}
+
+func (r Ranges) String() string {
+	content := ""
+	for _, spread := range r.r {
+		content += fmt.Sprintf("%d - %d => %d\n", spread.start, spread.end, (spread.end - spread.start + 1))
+	}
+
+	return content
 }
 
 func ParseInput(content string) (Ranges, []int) {
@@ -85,4 +106,64 @@ func FindIncludedValues(r Ranges, v []int) int {
 	}
 
 	return count
+}
+
+func CompactRanges(r Ranges) Ranges {
+	ranges := r.r
+	diff := len(ranges)
+	for diff > 0 {
+		beforeCount := len(ranges)
+		rangesWrap := CompactRangesSingle(ranges)
+		afterCount := len(rangesWrap.r)
+		diff = beforeCount - afterCount
+		ranges = rangesWrap.r
+	}
+
+	return Ranges{ranges}
+}
+
+func CompactRangesSingle(r []Range) Ranges {
+	newRanges := []*Range{}
+	for _, oldRange := range r {
+		found := false
+		for _, newRange := range newRanges {
+			if newRange.start == oldRange.start && newRange.end == oldRange.end {
+				found = true
+				continue
+			}
+			if oldRange.IsIncluded(newRange.start) || oldRange.IsIncluded(newRange.end) || newRange.IsIncluded(oldRange.start) || newRange.IsIncluded(oldRange.end) || oldRange.end+1 == newRange.start || oldRange.start == newRange.end+1 {
+				// merge the ranges
+				newStart := int(math.Min(float64(oldRange.start), float64(newRange.start)))
+				newEnd := int(math.Max(float64(oldRange.end), float64(newRange.end)))
+
+				newRange.start = newStart
+				newRange.end = newEnd
+
+				found = true
+			}
+		}
+		if !found {
+			newRanges = append(newRanges, &Range{oldRange.start, oldRange.end})
+		}
+	}
+
+	return Ranges{convertFromPointer(newRanges)}
+}
+
+func convertFromPointer(ptrs []*Range) []Range {
+	ranges := []Range{}
+	for _, r := range ptrs {
+		ranges = append(ranges, *r)
+	}
+
+	return ranges
+}
+
+func CountItemRanges(r Ranges) int {
+	sum := 0
+	for _, spread := range r.r {
+		sum += 1 + spread.end - spread.start
+	}
+
+	return sum
 }
--- 05/main_test.go
+++ 05/main_test.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	// "fmt"
+	// "os"
+	"fmt"
+	"os"
 	"testing"
 
 	aoclib "github.com/MattAMonroe/AdventOfCode2025/AOCLib"
@@ -28,9 +32,65 @@ func TestFullP1(t *testing.T) {
 func TestSampleP2(t *testing.T) {
 	content := aoclib.ReadFile("sample.txt")
 	assert.NotEqualf(t, "", content, "Failed to read in file contents for sample.txt")
+
+	ranges, _ := ParseInput(content)
+	newRanges := CompactRanges(ranges)
+	count := CountItemRanges(newRanges)
+	assert.Equal(t, 14, count)
+}
+
+func TestCompactRangesSample(t *testing.T) {
+	content := aoclib.ReadFile("sample.txt")
+	assert.NotEqualf(t, "", content, "Failed to read in file contents for sample.txt")
+
+	ranges, _ := ParseInput(content)
+	assert.Equal(t, 4, len(ranges.r))
+	newRanges := CompactRanges(ranges)
+	assert.Equal(t, 2, len(newRanges.r))
+}
+
+func TestCompactRangesTouching(t *testing.T) {
+	content := "3-4\n5-7"
+	ranges, _ := ParseInput(content)
+	assert.Equal(t, 2, len(ranges.r))
+	newRanges := CompactRanges(ranges)
+	assert.Equal(t, 1, len(newRanges.r))
+	count := CountItemRanges(newRanges)
+	assert.Equal(t, 5, count)
+}
+
+func TestRangesOverlap(t *testing.T) {
+	content := aoclib.ReadFile("problem.txt")
+	assert.NotEqualf(t, "", content, "Failed to read in file contents for problem.txt")
+
+	ranges, _ := ParseInput(content)
+	newRanges := CompactRanges(ranges)
+
+	for _, origRange := range ranges.r {
+		if newRanges.IsIncluded(origRange.start) && newRanges.IsIncluded(origRange.end) {
+			continue
+		}
+
+		fmt.Fprintf(os.Stderr, "Couldn't find new Range for original Range: %v\n", origRange)
+	}
+
+	for _, newRange := range newRanges.r {
+		if ranges.IsIncluded(newRange.start) && ranges.IsIncluded(newRange.end) {
+			continue
+		}
+
+		fmt.Fprintf(os.Stderr, "Couldn't find new Range for New Range: %v\n", newRange)
+	}
 }
 
 func TestFullP2(t *testing.T) {
 	content := aoclib.ReadFile("problem.txt")
 	assert.NotEqualf(t, "", content, "Failed to read in file contents for problem.txt")
+
+	ranges, _ := ParseInput(content)
+	newRanges := CompactRanges(ranges)
+
+	count := CountItemRanges(newRanges)
+	assert.Equal(t, 341753674214273, count)
+
 }
