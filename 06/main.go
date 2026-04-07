e236cbcd7cd17cc554f3fff3eae831608af9cce1
Matt Monroe <matt.a.monroe@gmail.com>
2026-04-07 10:17:44 -0600

    Day 6 - Part 1 - Solution

--- 06/main.go
+++ 06/main.go
@@ -1,7 +1,93 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	fmt.Printf("Hello World!")
+}
+
+type Operation int
+
+const (
+	OpAdd Operation = iota
+	OpMult
+	OpUnknown
+)
+
+type Problem struct {
+	Numbers []int
+	Op      Operation
+}
+
+func ParseGrid(content string) []Problem {
+	lines := strings.Split(content, "\n")
+	problems := []Problem{}
+	for _, line := range lines {
+		corrected := line
+		for strings.Contains(corrected, "  ") {
+			corrected = strings.ReplaceAll(corrected, "  ", " ")
+		}
+
+		if strings.ReplaceAll(corrected, " ", "") == "" {
+			continue
+		}
+
+		corrected = strings.Trim(corrected, " ")
+
+		nums := strings.Split(corrected, " ")
+		for i, entry := range nums {
+			if strings.ReplaceAll(entry, " ", "") == "" {
+				continue
+			}
+
+			if len(problems) <= i {
+				problems = append(problems, Problem{[]int{}, OpUnknown})
+			}
+
+			num, err := strconv.Atoi(entry)
+			if err != nil {
+				if entry == "*" {
+					problems[i].Op = OpMult
+				} else if entry == "+" {
+					problems[i].Op = OpAdd
+				} else {
+					fmt.Fprintf(os.Stderr, "Failed to parse entry %s: %v\n", entry, err)
+					continue
+				}
+				continue
+			}
+			problems[i].Numbers = append(problems[i].Numbers, num)
+		}
+	}
+	return problems
+}
+
+func ComputeMath(problems []Problem) int {
+	sum := 0
+	for _, problem := range problems {
+		value := 0
+
+		switch problem.Op {
+		case OpMult:
+			value = problem.Numbers[0]
+			for _, p := range problem.Numbers[1:] {
+				value *= p
+			}
+		case OpAdd:
+			for _, p := range problem.Numbers {
+				value += p
+			}
+		default:
+			fmt.Fprintf(os.Stderr, "Unknown Operation\n")
+		}
+
+		sum += value
+	}
+
+	return sum
 }
--- 06/main_test.go
+++ 06/main_test.go
@@ -10,11 +10,22 @@ import (
 func TestSampleP1(t *testing.T) {
 	content := aoclib.ReadFile("sample.txt")
 	assert.NotEqualf(t, "", content, "Failed to read in file contents for sample.txt")
+
+	problems := ParseGrid(content)
+	assert.Equal(t, 4, len(problems))
+
+	sum := ComputeMath(problems)
+	assert.Equal(t, 4277556, sum)
 }
 
 func TestFullP1(t *testing.T) {
 	content := aoclib.ReadFile("problem.txt")
 	assert.NotEqualf(t, "", content, "Failed to read in file contents for problem.txt")
+
+	problems := ParseGrid(content)
+
+	sum := ComputeMath(problems)
+	assert.Equal(t, 5171061464548, sum)
 }
 
 func TestSampleP2(t *testing.T) {
