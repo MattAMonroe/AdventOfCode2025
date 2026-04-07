e4bc790d01a29a2d3ae6a3e94a7cf79042bd9665
Matt Monroe <matt.a.monroe@gmail.com>
2026-04-07 11:21:45 -0600

    Day 6 - Part 2 - Solution

--- 06/main.go
+++ 06/main.go
@@ -67,6 +67,92 @@ func ParseGrid(content string) []Problem {
 	return problems
 }
 
+func ParseVertical(content string) []Problem {
+	problems := []Problem{}
+
+	grid := parseGrid(content)
+
+	opRow := len(grid) - 1
+	for i := range len(grid[0]) - 1 {
+		switch grid[opRow][i] {
+		case "*":
+			nextOp := findNextOpOffset(grid[opRow][i+1:]) + 1 + i
+			if nextOp == i {
+				nextOp = len(grid[opRow]) + 1
+			}
+			spread := nextOp - 1 - i
+			problem := Problem{[]int{}, OpMult}
+			for j := range spread {
+				col := nextOp - 2 - j
+				num := readIntFromColumn(grid, col)
+				problem.Numbers = append(problem.Numbers, num)
+			}
+			problems = append(problems, problem)
+		case "+":
+			nextOp := findNextOpOffset(grid[opRow][i+1:]) + 1 + i
+			if nextOp == i {
+				nextOp = len(grid[opRow]) + 1
+			}
+			spread := nextOp - 1 - i
+			problem := Problem{[]int{}, OpAdd}
+			for j := range spread {
+				col := nextOp - 2 - j
+				num := readIntFromColumn(grid, col)
+				problem.Numbers = append(problem.Numbers, num)
+			}
+			problems = append(problems, problem)
+		default:
+			continue
+		}
+	}
+
+	return problems
+}
+
+func parseGrid(content string) [][]string {
+	lines := strings.Split(content, "\n")
+	grid := [][]string{}
+
+	for _, line := range lines {
+		if strings.Trim(line, " ") == "" {
+			continue
+		}
+		slice := []string{}
+		for _, digit := range line {
+			slice = append(slice, string(digit))
+		}
+
+		grid = append(grid, slice)
+	}
+
+	return grid
+}
+
+func findNextOpOffset(row []string) int {
+	for i := range len(row) {
+		if row[i] == "*" || row[i] == "+" {
+			return i
+		}
+	}
+	return -1
+}
+
+func readIntFromColumn(grid [][]string, col int) int {
+	// read the vertical column into an integer and return it
+	numStr := ""
+	for i := range len(grid) - 1 {
+		numStr += grid[i][col]
+	}
+
+	numStr = strings.Trim(numStr, " ")
+	num, err := strconv.Atoi(numStr)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to parse column %d: %s\n", col, numStr)
+	}
+
+	return num
+}
+
 func ComputeMath(problems []Problem) int {
 	sum := 0
 	for _, problem := range problems {
--- 06/main_test.go
+++ 06/main_test.go
@@ -28,12 +28,49 @@ func TestFullP1(t *testing.T) {
 	assert.Equal(t, 5171061464548, sum)
 }
 
+func TestRedColumns(t *testing.T) {
+	content := aoclib.ReadFile("sample.txt")
+	assert.NotEqualf(t, "", content, "Failed to read in file contents for sample.txt")
+
+	grid := parseGrid(content)
+
+	assert.Equal(t, 1, readIntFromColumn(grid, 0))
+	assert.Equal(t, 24, readIntFromColumn(grid, 1))
+	assert.Equal(t, 356, readIntFromColumn(grid, 2))
+	assert.Equal(t, 369, readIntFromColumn(grid, 4))
+
+	assert.Equal(t, 248, readIntFromColumn(grid, 5))
+	assert.Equal(t, 8, readIntFromColumn(grid, 6))
+	assert.Equal(t, 32, readIntFromColumn(grid, 8))
+	assert.Equal(t, 581, readIntFromColumn(grid, 9))
+
+	nextIdx := findNextOpOffset(grid[len(grid)-1][1:])
+	assert.Equal(t, 3, nextIdx)
+
+	nextIdx = findNextOpOffset(grid[len(grid)-1][5:])
+	assert.Equal(t, 3, nextIdx)
+
+	nextIdx = findNextOpOffset(grid[len(grid)-1][9:])
+	assert.Equal(t, 3, nextIdx)
+
+	nextIdx = findNextOpOffset(grid[len(grid)-1][13:])
+	assert.Equal(t, -1, nextIdx)
+}
+
 func TestSampleP2(t *testing.T) {
 	content := aoclib.ReadFile("sample.txt")
 	assert.NotEqualf(t, "", content, "Failed to read in file contents for sample.txt")
+
+	problems := ParseVertical(content)
+	sum := ComputeMath(problems)
+	assert.Equal(t, 3263827, sum)
 }
 
 func TestFullP2(t *testing.T) {
 	content := aoclib.ReadFile("problem.txt")
 	assert.NotEqualf(t, "", content, "Failed to read in file contents for problem.txt")
+
+	problems := ParseVertical(content)
+	sum := ComputeMath(problems)
+	assert.Equal(t, 10189959087258, sum)
 }
