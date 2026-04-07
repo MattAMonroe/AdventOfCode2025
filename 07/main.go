55d6e56c684d0b03e25c2d2360313bad469d6d67
Matt Monroe <matt.a.monroe@gmail.com>
2026-04-07 11:49:05 -0600

    Day 7 - Part 1 - Solution

--- 07/main.go
+++ 07/main.go
@@ -1,7 +1,73 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
 	fmt.Printf("Hello World!")
+}
+
+func ParseGrid(content string) [][]string {
+	lines := strings.Split(strings.Trim(content, " "), "\n")
+
+	grid := [][]string{}
+	for _, line := range lines {
+		row := []string{}
+		if strings.Trim(line, " ") == "" {
+			continue
+		}
+
+		for _, char := range line {
+			row = append(row, string(char))
+		}
+
+		grid = append(grid, row)
+	}
+
+	return grid
+}
+
+func CountBeams(grid [][]string) int {
+	countSplits := 0
+
+	beamStart := GetBeamStart(grid[0])
+	if beamStart == -1 {
+		fmt.Fprintf(os.Stderr, "Failed to find Beam Start\n")
+		return -1
+	}
+	beams := map[int]bool{}
+	beams[beamStart] = true
+	for _, row := range grid[1:] {
+		newBeams := map[int]bool{}
+		for beam := range beams {
+			if row[beam] == "^" {
+				if beam-1 >= 0 {
+					newBeams[beam-1] = true
+				}
+				if beam+1 < len(row) {
+					newBeams[beam+1] = true
+				}
+				countSplits += 1
+			} else {
+				newBeams[beam] = true
+			}
+		}
+
+		beams = newBeams
+	}
+
+	return countSplits
+}
+
+func GetBeamStart(row []string) int {
+	for i, char := range row {
+		if char == "S" {
+			return i
+		}
+	}
+
+	return -1
 }
--- 07/main_test.go
+++ 07/main_test.go
@@ -10,11 +10,21 @@ import (
 func TestSampleP1(t *testing.T) {
 	content := aoclib.ReadFile("sample.txt")
 	assert.NotEqualf(t, "", content, "Failed to read in file contents for sample.txt")
+
+	grid := ParseGrid(content)
+	assert.Greater(t, len(grid), 1)
+	splits := CountBeams(grid)
+	assert.Equal(t, 21, splits)
 }
 
 func TestFullP1(t *testing.T) {
 	content := aoclib.ReadFile("problem.txt")
 	assert.NotEqualf(t, "", content, "Failed to read in file contents for problem.txt")
+
+	grid := ParseGrid(content)
+	assert.Greater(t, len(grid), 1)
+	splits := CountBeams(grid)
+	assert.Equal(t, 1687, splits)
 }
 
 func TestSampleP2(t *testing.T) {
