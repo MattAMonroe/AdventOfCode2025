cd1398b6f2acedb07af9a66e18bb881205fa6e5b
Matt Monroe <matt.a.monroe@gmail.com>
2026-04-14 10:37:57 -0600

    Day 9 - Part 1 - Solution

--- 09/main.go
+++ 09/main.go
@@ -1,7 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	fmt.Printf("Hello World!")
+}
+
+type Point struct {
+	X int
+	Y int
+}
+
+func (p Point) GetArea(o Point) int {
+	x := abs(p.X-o.X) + 1
+	y := abs(p.Y-o.Y) + 1
+	return x * y
+}
+
+func abs(i int) int {
+	if i < 0 {
+		return i * -1
+	}
+	return i
+}
+
+func ParseInput(content string) []Point {
+	points := []Point{}
+	splits := strings.Split(strings.Trim(content, " "), "\n")
+	for _, split := range splits {
+		if strings.Trim(split, " ") == "" {
+			continue
+		}
+		coords := strings.Split(split, ",")
+		if len(coords) != 2 {
+			fmt.Fprintf(os.Stderr, "Failed to parse coords: %s\n", split)
+			continue
+		}
+		x, err := strconv.Atoi(coords[0])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to parse coords.X: %s\n", coords[0])
+			continue
+		}
+		y, err := strconv.Atoi(coords[1])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to parse coords.Y: %s\n", coords[1])
+			continue
+		}
+		points = append(points, Point{x, y})
+	}
+
+	return points
+}
+
+func FindMaxArea(points []Point) int {
+	largest := 0
+	for i, p1 := range points {
+		for _, p2 := range points[i+1:] {
+			area := p1.GetArea(p2)
+			if area > largest {
+				largest = area
+			}
+		}
+	}
+
+	return largest
 }
--- 09/main_test.go
+++ 09/main_test.go
@@ -10,11 +10,58 @@ import (
 func TestSampleP1(t *testing.T) {
 	content := aoclib.ReadFile("sample.txt")
 	assert.NotEqualf(t, "", content, "Failed to read in file contents for sample.txt")
+
+	points := ParseInput(content)
+	assert.Equal(t, 8, len(points))
+
+	area := FindMaxArea(points)
+	assert.Equal(t, 50, area)
+}
+
+func TestGetArea(t *testing.T) {
+	p1 := Point{2, 5}
+	p2 := Point{9, 7}
+	p1p2Area := p1.GetArea(p2)
+	assert.Equal(t, 24, p1p2Area)
+
+	p2p1Area := p2.GetArea(p1)
+	assert.Equal(t, 24, p2p1Area)
+
+	p3 := Point{7, 1}
+	p4 := Point{11, 7}
+	p3p4Area := p3.GetArea(p4)
+	assert.Equal(t, 35, p3p4Area)
+
+	p4p3Area := p4.GetArea(p3)
+	assert.Equal(t, 35, p4p3Area)
+
+	p5 := Point{7, 3}
+	p6 := Point{2, 3}
+	p5p6Area := p5.GetArea(p6)
+	assert.Equal(t, 6, p5p6Area)
+
+	p6p5Area := p6.GetArea(p5)
+	assert.Equal(t, 6, p6p5Area)
+
+	p7 := Point{2, 5}
+	p8 := Point{11, 1}
+	p7p8Area := p7.GetArea(p8)
+	assert.Equal(t, 50, p7p8Area)
+
+	p8p7Area := p8.GetArea(p7)
+	assert.Equal(t, 50, p8p7Area)
+
 }
 
 func TestFullP1(t *testing.T) {
 	content := aoclib.ReadFile("problem.txt")
 	assert.NotEqualf(t, "", content, "Failed to read in file contents for problem.txt")
+
+	points := ParseInput(content)
+	assert.Equal(t, 496, len(points))
+
+	area := FindMaxArea(points)
+	assert.Equal(t, 4741451444, area)
 }
 
 func TestSampleP2(t *testing.T) {
